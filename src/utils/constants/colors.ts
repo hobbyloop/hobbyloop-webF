534e305f7f36daa411f54b587b145166b49dca70
yoyongjin <yongjinalize@gmail.com>
2023-09-17 02:12:50 +0900

    💄 Add: Colors 추가
    폰트 관련 색상 추가

--- src/utils/constants/colors.ts
+++ src/utils/constants/colors.ts
@@ -1,5 +1,11 @@
-// 버튼 색상
 export enum Colors {
+  // 버튼 색상
   orange = "#FF5F05",
   white = "#FFFFFF",
+  green = "#5B8B4B",
+
+  // 폰트 색상
+  placeholder = "#6C6C6C",
+  inputLabel = "#141414",
+  indicator = "#EB2323",
 }
