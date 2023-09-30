baa6d7758ddab35945ef011c34ddf51f3926aab6
yoyongjin <yongjinalize@gmail.com>
2023-10-01 05:22:24 +0900

    💄 Add: 관리자/강사 로그인 화면 추가
    useNavigate -> useNavigation 추후 고려

---
+++ src/pages/AdminLoginPage.tsx
@@ -0,0 +1,8 @@
+import LoginTemplate from "components/common/templates/LoginTemplate";
+import React from "react";
+
+function AdminLoginPage() {
+  return <LoginTemplate authority="관리자" />;
+}
+
+export default AdminLoginPage;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,17 @@
 import Logo from "components/common/atoms/Logo";
 import Header from "components/common/templates/Header";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { Colors } from "utils/constants/colors";
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: space-between;
 
   width: 100vw;
-  background-color: red;
 `;
 
 const LoginButton = styled.button`
@@ -29,16 +30,42 @@ const ButtonsWrapper = styled.div`
   gap: 22px;
 `;
 
+const Body = styled.div`
+  display: flex;
+`;
+
+const Introduce1 = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 791px;
+  height: 503px;
+`;
+const Introduce2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 384px;
+  height: 503px;
+`;
+
 function HomePage() {
+  const navigate = useNavigate();
   return (
     <Container>
       <Header>
         <Logo />
         <ButtonsWrapper>
-          <LoginButton>관리자 로그인</LoginButton>
-          <LoginButton>강사 로그인</LoginButton>
+          <LoginButton onClick={() => navigate("/login_admin")}>
+            관리자 로그인
+          </LoginButton>
+          <LoginButton onClick={() => navigate("/login_instructor")}>
+            강사 로그인
+          </LoginButton>
         </ButtonsWrapper>
       </Header>
+      <Body>
+        <Introduce1>프로모션</Introduce1>
+        <Introduce2>가격정보</Introduce2>
+      </Body>
     </Container>
   );
 }
