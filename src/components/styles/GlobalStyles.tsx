3175174fe13c5266a6589d0a464544e376d63624
yoyongjin <yongjinalize@gmail.com>
2023-10-01 04:34:17 +0900

    🔥 Remove: 불필요 CSS 코드 삭제

--- src/components/styles/GlobalStyles.tsx
+++ src/components/styles/GlobalStyles.tsx
@@ -2,15 +2,5 @@ import { createGlobalStyle } from "styled-components";
 import { reset } from "styled-reset";
 
 export const GlobalStyles = createGlobalStyle`
-    ${reset}
-    html, body {
-    padding: 0;
-  }
-  div, input {
-    box-sizing: border-box;
-  }
-  * {
-    padding: 0;
-    margin: 0;
-  }
+    ${reset}   
 `;
