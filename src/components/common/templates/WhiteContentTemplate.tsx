f90c2559f5232853dd14a89279a5c31c20dcdf6a
yoyongjin <yongjinalize@gmail.com>
2023-10-03 19:08:29 +0900

    💄 Add: WhiteContentTemplate 컴포넌트 추가(templates)
    Header + WhiteWrapper 있는 컴포넌트 template으로 만든거

---
+++ src/components/common/templates/WhiteContentTemplate.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+import { Colors } from "utils/constants/colors";
+import Header from "./Header";
+
+export interface WhiteContentTemplateProps {
+  headerElements?: React.ReactElement;
+  children: React.ReactNode;
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: ${Colors.app};
+`;
+
+const WhiteWrapper = styled.div`
+  width: 792px;
+  margin-top: 48px;
+  margin-bottom: 120px;
+  background-color: ${Colors.white};
+`;
+
+function WhiteContentTemplate({
+  headerElements,
+  children,
+}: WhiteContentTemplateProps) {
+  return (
+    <Container>
+      <Header>logo{headerElements}</Header>
+      <WhiteWrapper>{children}</WhiteWrapper>
+    </Container>
+  );
+}
+
+export default WhiteContentTemplate;
