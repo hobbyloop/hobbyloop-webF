b0015b1a0f9cf2b717730fbeb42d9de165a28ddc
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2023-10-29 23:38:28 +0900

    💄 Add: TextArea Atom 제작

--- src/components/common/atoms/TextArea.tsx
+++ src/components/common/atoms/TextArea.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { TextareaHTMLAttributes } from "react";
+import styled from "styled-components";
+import { Colors } from "utils/constants/colors";
 
-function TextArea() {
-  return <div>TextArea</div>;
-}
+interface Props extends TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const TextArea = ({ ...rest }: Props) => {
+  return <Container {...rest} />;
+};
 
 export default TextArea;
+
+const Container = styled.textarea`
+  width: 100%;
+  height: 123px;
+  min-height: 123px;
+  font-size: 16px;
+  line-height: 24px;
+  letter-spacing: -0.02em;
+  color: ${Colors.black};
+  border-radius: 8px;
+  box-sizing: border-box;
+  border: 1px solid ${Colors.gray};
+  resize: none;
+  font-weight: 500;
+  padding: 12px 20px;
+  outline: none;
+  font-family: "Pretendard"; // TODO 전체 스타일로 빠지면 제거
+  scroll-padding: 12px;
+
+  &::placeholder {
+    color: ${Colors.placeholder};
+  }
+`;
