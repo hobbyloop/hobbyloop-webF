67143b40c733e1a27ead7418495369dc49b593c2
datawn-jy <cjy0029@data-wn.com>
2023-11-03 10:32:30 +0900

    📦 add : storybook addon 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,20 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../src/**/*.stories.(js|mdx)",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    "@storybook/addon-backgrounds", // 화면 밝기 어둡기 조절
+    "@storybook/addon-knobs/register", // knobs는 컴포넌트의 props를 스토리북 화면에서 바로 반영시켜주는
+    "@storybook/addon-docs",
   ],
   framework: {
     name: "@storybook/react-webpack5",
